283a640f265f729fbea60e3ac8a42a6f8921657d
Mopenzi <merny.cichal@seznam.cz>
2025-03-06 17:17:28 +0100

    přidány výpisy podle zadání 1. části cvičení - indexy

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 word = "indexování"
 
+print("Prvních 5 písmen: ")
+print(word[:5])
+print("Posledních 5 písmen: ")
+print(word[-5:])
+print("Každé 3. písmeno (počínaje prvním) od 'i': ")
+print(word[::3])
+
