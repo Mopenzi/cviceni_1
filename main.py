5dc3f17a3d44afc58c33a3298b07419a4a679e55
Mopenzi <merny.cichal@seznam.cz>
2025-03-07 09:27:10 +0100

    zakomentoval jsem prvni cast cviceni

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-word = "indexování"
+# word = "indexování"
 
-print("Prvních 5 písmen: ")
-print(word[:5])
-print("Posledních 5 písmen: ")
-print(word[-5:])
-print("Každé 3. písmeno (počínaje prvním) od 'i': ")
-print(word[::3])
+# print("Prvních 5 písmen: ")
+# print(word[:5])
+# print("Posledních 5 písmen: ")
+# print(word[-5:])
+# print("Každé 3. písmeno (počínaje prvním) od 'i': ")
+# print(word[::3])
 
