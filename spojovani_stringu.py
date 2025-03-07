9ce2ac099e6c731ce5b053aa7bfdfec640cc9f93
Mopenzi <merny.cichal@seznam.cz>
2025-03-07 10:28:17 +0100

    Pridal jsem soubor spojovani stringu a vypracoval ukol podle zadani

---
+++ spojovani_stringu.py
@@ -0,0 +1,9 @@
+jmeno = "Lukáš"
+prijmeni = "Dvořák"
+cele_jmeno = jmeno + " " + prijmeni
+delka_jmena = len(cele_jmeno)
+print("Celé jméno:", cele_jmeno)
+print("Délka jména:", delka_jmena)
+print("=" * delka_jmena)
+print(cele_jmeno)
+print("=" * delka_jmena)
